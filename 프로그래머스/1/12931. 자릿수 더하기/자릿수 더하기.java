c2108720427cc7ec7416e92500d303f33b795dd0
Yeji Kim <yejikim.erica@gmail.com>
2025-07-02 08:26:42 +0900

    [level 1] Title: 자릿수 더하기, Time: 7.69 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String[] nArr = (n+"").split("");
+        for (int i = 0; i < nArr.length; i++) {
+			answer += Integer.parseInt(nArr[i]);
+		}
+        
+        return answer;
+    }
+}
