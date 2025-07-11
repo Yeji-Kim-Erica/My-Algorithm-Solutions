3611fda24a79e263f0655c7fa8007d7feac4664b
Yeji Kim <yejikim.erica@gmail.com>
2025-07-11 09:44:31 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+		
+	} // main
+	
+} // class
