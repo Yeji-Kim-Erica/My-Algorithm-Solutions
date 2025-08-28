dff616f2d0f84e9e4f5d69296ae81aa52d78a5b3
Yeji Kim <yejikim.erica@gmail.com>
2025-08-28 11:56:17 +0900

    문제 풀이 목록 업데이트 (풀이 과정이 담긴 노션 링크 첨부)

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# My-Algorithm-Solutions
-알고리즘 문제 풀이 및 솔루션 코드 모음
+# My-Algorithm-Solutions : 알고리즘 문제 풀이 및 솔루션 코드 모음
+
+이 저장소는 백준 온라인 저지 문제 풀이 코드를 자동으로 기록합니다.
+
+문제에 대한 상세한 분석 및 풀이 과정은 아래 링크된 노션 페이지에 정리되어 있습니다.
+
+---
+
+### 문제 풀이 목록
+
+| 문제 번호 | 문제 이름 | 코드 보기 | 상세 풀이 (Notion) |
+| :------: | :-------: | :-------: | :----------------: |
+|   1302   | 베스트셀러 | [Java](https://github.com/Yeji-Kim-Erica/My-Algorithm-Solutions/blob/main/%EB%B0%B1%EC%A4%80/Silver/1302.%E2%80%85%EB%B2%A0%EC%8A%A4%ED%8A%B8%EC%85%80%EB%9F%AC/%EB%B2%A0%EC%8A%A4%ED%8A%B8%EC%85%80%EB%9F%AC.java) | [Link📝](https://www.notion.so/1302-25bdc4b46fd5801dbf3eea6eb28cbe1f?source=copy_link) |
+|   10816  |  숫자 카드 2 | [Java](https://github.com/Yeji-Kim-Erica/My-Algorithm-Solutions/blob/main/%EB%B0%B1%EC%A4%80/Silver/10816.%E2%80%85%EC%88%AB%EC%9E%90%E2%80%85%EC%B9%B4%EB%93%9C%E2%80%852/%EC%88%AB%EC%9E%90%E2%80%85%EC%B9%B4%EB%93%9C%E2%80%852.java) | [Link📝](https://www.notion.so/10816-2-25ddc4b46fd580a4a661f0f293d55655?source=copy_link) |
+|   ...    |    ...    |    ...    |        ...         |
--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -3,41 +3,33 @@ import java.util.Map.Entry;
 import java.io.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		
-		TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>();
-		String book;
-		try {
-			int number = Integer.parseInt(reader.readLine());
-			for (int i = 0; i < number; i++) {
-				book = reader.readLine();
-				if (treeMap.containsKey(book)) {
-					treeMap.replace(book, treeMap.get(book)+1);
-				} else {
-					treeMap.put(book, 1);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		Set<Entry<String, Integer>> set = treeMap.entrySet();
-		Iterator<Entry<String, Integer>> it = set.iterator();
-		int maxValue = 0;
-		String answer = "";
-		while (it.hasNext()) {
-			Entry<String, Integer> entry = (Entry<String, Integer>) it.next();
-			if (entry.getValue() > maxValue) {
-				maxValue = entry.getValue();
-				answer = entry.getKey();
-			}
-		}
-		
-		System.out.println(answer);
-
-	} // main
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        // Key(책 제목)가 자동으로 사전순 정렬되는 TreeMap 사용
+        TreeMap<String, Integer> sales = new TreeMap<>();
+        
+        int n = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < n; i++) {
+            String book = reader.readLine();
+            // getOrDefault를 사용해 간결하게 판매량 집계
+            sales.put(book, sales.getOrDefault(book, 0) + 1);
+        }
+        
+        int maxValue = 0;
+        String answer = "";
+        
+        // for-each 구문으로 깔끔하게 순회
+        // TreeMap이므로 entry는 이미 책 제목의 사전순으로 정렬된 상태
+        for (Entry<String, Integer> entry : sales.entrySet()) {
+            if (entry.getValue() > maxValue) {
+                maxValue = entry.getValue();
+                answer = entry.getKey();
+            }
+        }
+        
+        System.out.println(answer);
+        
+    } // main
+    
 } // class
