71e3873183dd77aa64a35e374fe71571926650a1
Yeji Kim <yejikim.erica@gmail.com>
2025-09-10 11:51:43 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,59 +1,51 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
-        // 스위치 상태 저장
-        int switchNum = Integer.parseInt(reader.readLine());
-        boolean[] switchArr = new boolean[switchNum+1]; // 스위치 번호에 매번 1 빼서 혼란 생기는 걸 막기 위해
+        int n = Integer.parseInt(reader.readLine());
+        // 1번 인덱스부터 사용하기 위해 n+1 크기로 생성
+        boolean[] switches = new boolean[n + 1]; 
         StringTokenizer st = new StringTokenizer(reader.readLine());
-        for (int i = 1; i < switchArr.length; i++) { // 스위치 개수가 고정되어 있으므로 for문
-			switchArr[i] = st.nextToken().equals("1"); // 1이면 true, 0이면 false
-		}
-        
-        // 학생
-        int studentNum = Integer.parseInt(reader.readLine());
-        for (int i = 0; i < studentNum; i++) { // 학생 수가 고정되어 있으므로 for문
-        	st = new StringTokenizer(reader.readLine());
-        	String gender = st.nextToken();
-        	int givenNum = Integer.parseInt(st.nextToken());
-        	if (gender.equals("1")) { // 남자일 경우
-        		for (int j = givenNum; j < switchArr.length; j+=givenNum) {
-					switchArr[j] = !switchArr[j];
-				}
-			} else if (gender.equals("2")) { // 여자일 경우
-				int rightDistance = Math.min(switchArr.length - 1, givenNum*2) - givenNum;
-				int leftDistance = givenNum - Math.max(1, givenNum - rightDistance);
-				int distance = Math.min(rightDistance, leftDistance);
-				for (int j = 1; j <= distance; j++) {
-					if (switchArr[givenNum + j] == switchArr[givenNum - j]) {
-						switchArr[givenNum + j] = !switchArr[givenNum + j];
-						switchArr[givenNum - j] = !switchArr[givenNum - j];
-					} else {
-						break;
-					}
-				}
-				switchArr[givenNum] = !switchArr[givenNum];
-			}
+        for (int i = 1; i <= n; i++) {
+            switches[i] = st.nextToken().equals("1");
         }
         
-        StringBuffer sb = new StringBuffer();
-        for (int i = 1; i < switchArr.length; i++) {
-			if (switchArr[i]) {
-				sb.append("1 ");
-			} else {
-				sb.append("0 ");
-			}
-			if (i%20==0) {
-				System.out.println(sb.toString().trim());
-				sb = new StringBuffer();
-			}
-		}
-        if (sb.length() > 0) {
-        	System.out.println(sb.toString().trim());
-		}
+        int studentCount = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < studentCount; i++) {
+            st = new StringTokenizer(reader.readLine());
+            int gender = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            
+            if (gender == 1) { // 남학생
+                for (int j = num; j <= n; j += num) {
+                    switches[j] = !switches[j];
+                }
+            } else { // 여학생
+                switches[num] = !switches[num]; // 중심 스위치 먼저 변경
+                int d = 1;
+                while (num - d >= 1 && num + d <= n) {
+                    if (switches[num - d] == switches[num + d]) {
+                        switches[num - d] = !switches[num - d];
+                        switches[num + d] = !switches[num + d];
+                        d++;
+                    } else {
+                        break; // 대칭이 깨지면 중단
+                    }
+                }
+            }
+        }
+        
+        // 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(switches[i] ? "1" : "0").append(" ");
+            if (i % 20 == 0) {
+                sb.append("\n");
+            }
+        }
+        System.out.println(sb.toString().trim());
     }
 }
