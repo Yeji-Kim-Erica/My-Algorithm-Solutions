003b66bcf76ee3d8b224d800a9380b0b837e5129
Yeji-Kim-Erica <yejikim.erica@gmail.com>
2026-03-23 11:11:08 +0900

    [level 0] Title: 컨트롤 제트, Time: 1.47 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,39 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
-    	String[] sArr = s.split("\\s");
-        
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < sArr.length ; i++) {
-			if (sArr[i].equals("Z")) {
-				stack.pop();
-			} else {
-				stack.push(Integer.parseInt(sArr[i]));
-			}
-		}
-        
+        String[] numbers = s.split("-*\\d+\\sZ|\\s");
         int answer = 0;
-        for (int n : stack) {
-        	answer += n;
-		}
-        
+        for (int i = 0; i < numbers.length; i++) {
+            if (!numbers[i].isEmpty())
+            answer += Integer.parseInt(numbers[i]);
+        }
         return answer;
     }
-    
-/* 이전 풀이
-    public int solution(String s) {
-    	String[] sArr = s.split("\\s");
-        int answer = 0;
-        for (int i = sArr.length - 1; i >= 0 ; i--) {
-			if (sArr[i].equals("Z")) {
-				sArr[i - 1] = "0";
-			} else {
-				answer += Integer.parseInt(sArr[i]);
-			}
-		}
-        return answer;
-    }
-*/
-    
 }
