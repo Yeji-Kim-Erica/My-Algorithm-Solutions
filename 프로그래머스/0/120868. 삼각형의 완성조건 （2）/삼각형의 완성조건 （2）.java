d1a23addd5ed881ecbcc30786dcd34c98de1ffc6
Yeji-Kim-Erica <yejikim.erica@gmail.com>
2026-04-05 17:48:46 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 1.26 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -1,27 +1,24 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] sides) {
-    	// 순서 상관 X, 중복 허용 X -> HashSet
-    	Set<Integer> lines = new HashSet<Integer>();
-    	
-    	// 배열 sides의 두 변 중 하나가 가장 긴 변일 때
-    	int min = Math.min(sides[0], sides[1]);
-    	int max = Math.max(sides[0], sides[1]);
-    	int line = max; // 나머지 한 변
-    	
-    	while (min + line > max) {
-    		lines.add(line);
-			line--;
-		}
-    	
-    	// 나머지 한 변이 가장 긴 변일 때
-    	line = max + 1;
-    	while (min + max > line) {
-			lines.add(line);
-			line++;
-		}
-    	
-        return lines.size();
+        Arrays.sort(sides);
+        
+        int answer = 0;
+        for (int i = sides[1]; i >= 1; i--) {
+            if (i + sides[0] <= sides[1]) {
+                break;
+            }
+            answer++;
+        }
+        
+        int sum = sides[0] + sides[1];
+        for (int i = sides[1] + 1; i < sum; i++) {
+            if (sum <= i) {
+                break;
+            }
+            answer++;
+        }
+        return answer;
     }
 }
